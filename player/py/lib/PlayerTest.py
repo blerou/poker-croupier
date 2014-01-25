12bbc2f72f75dda70fc3d62a8d72d156a8a46b1b
Balazs Vendeg <balazs.vendeg@gmail.com>
2014-01-25 17:34:13 +0100

    player test

--- player/py/lib/PlayerTest.py
+++ player/py/lib/PlayerTest.py
@@ -3,12 +3,53 @@ from handler import PlayerHandler
 __author__ = 'Balazs Vendeg'
 
 import unittest
-
+from api.ThriftTypes.ttypes import Card, BetLimits
 
 class PlayerTest(unittest.TestCase):
 
     def setUp(self):
-        self. player = PlayerHandler()
+        self.player = PlayerHandler()
 
-    def test(self):
-        self.assertTrue(True)
+    def test_all_in_with_ace_pair_in_hand(self):
+        c1 = Card()
+        c2 = Card()
+        c1.value = 14
+        c2.value = 14
+
+        self.player.hole_card(c1)
+        self.player.hole_card(c2)
+
+        l = BetLimits()
+        l.minimum_raise = 10
+        l.to_call = 10
+        self.assertEqual(self.player.bet_request(100, l), 1000)
+
+
+    def test_fold(self):
+        c1 = Card()
+        c2 = Card()
+        c1.value = 8
+        c2.value = 8
+
+        self.player.hole_card(c1)
+        self.player.hole_card(c2)
+
+        l = BetLimits()
+        l.minimum_raise = 10
+        l.to_call = 10
+        #self.assertEqual(self.player.bet_request(10, l), 0)
+
+    def test_no_fold_after_flop(self):
+
+      c1 = Card()
+      c2 = Card()
+      c1.value = 12
+      c2.value = 13
+
+      self.player.hole_card(c1)
+      self.player.hole_card(c2)
+
+      l = BetLimits()
+      l.minimum_raise = 10
+      l.to_call = 10
+      self.assertEqual(self.player.bet_request(10, l), 20)
