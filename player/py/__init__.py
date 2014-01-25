c869d9275a7899500dcab00e6f933707146a1ff6
Balazs Vendeg <balazs.vendeg@gmail.com>
2014-01-25 13:46:21 +0100

    .

---
+++ player/py/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'Balazs Vendeg'
--- player/py/lib/handler.py
+++ player/py/lib/handler.py
@@ -1,34 +1,82 @@
 import sys
 import os
 
+sys.path.append(os.path.abspath('lib'))
+
+from api.ThriftTypes.ttypes import BetType
+
 class PlayerHandler:
+
+  def __init__(self):
+
+    self.__reset__()
+    self.money = 1000
 
   def name(self):
     return os.environ['USER']
 
   def competitor_status(self, competitor):
-    pass
+    if len(self.my_cards) == 0 and competitor.name == self.name():
+      self.money += competitor.stack
 
   def hole_card(self, card):
-    pass
+    self.my_cards.append(card)
 
   def community_card(self, card):
-    pass
+    self.community_cards.append(card)
 
   def bet(self, competitor, bet):
+    #if self.__state__() == 2 and self.__eval__() > 24:
     pass
 
   def bet_request(self, pot, limits):
-    try:
-        return limits["to_call"]
-    except:
+    if self.__state__() == 2:
+      if self.__eval__() > 20:
+        return limits.to_call * 2
+      else:
         return 0
+
+    if self.__state__() == 5:
+      if self.__eval__() > 30:
+        return limits.to_call
+      else:
+        return 0
+
+    return limits.to_call;
 
   def showdown(self, comptetior, cards, hand):
     pass
 
   def winner(self, competitor, amount):
-    pass
+    self.__reset__()
+
 
   def shutdown(self):
       sys.exit('Shutting down server')
+<<<<<<< HEAD
+=======
+
+
+  def __state__(self):
+    return len(self.my_cards) + len(self.community_cards)
+
+  def __reset__(self):
+    self.my_cards = []
+    self.community_cards = []
+
+  def __eval_hand__(self):
+    value =  self.my_cards[0].value + self.my_cards[1].value
+    if (self.my_cards[0].value == self.my_cards[1].value):
+      value += 10
+    return value
+
+  def __eval__(self):
+    value = self.__eval_hand__()
+    if self.community_cards[0].value == self.community_cards[1].value:
+      value += self.community_cards[0].value * 2
+    if self.community_cards[1].value == self.community_cards[2].value:
+      value += self.community_cards[1].value * 2
+    if self.community_cards[0].value == self.community_cards[2].value:
+      value += self.community_cards[0].value * 2
+    return value
+>>>>>>> .
