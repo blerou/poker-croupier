9f7751000b149b72bdeea3d431795ed10b4c047a
blerou <sulik.szabolcs@ustream.tv>
2014-01-25 11:47:07 +0100

    fix merge

--- croupier/lib/game_steps/betting.rb
+++
@@ -1,8 +0,0 @@
-class Croupier::GameSteps::Betting
-  def run(game_state)
-    game_state.players.each do |_|
-      game_state.transfer_bet game_state.player_in_action, game_state.player_in_action.bet_request, :raise
-      game_state.next_player
-    end
-  end
-end
